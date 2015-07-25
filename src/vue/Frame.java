f93619781feb86903bb9a5d92ce70744850c4763
Antoine Busque <abusque@efficios.com>
2015-07-25 15:46:19 -0400

    Clean-up main classes

--- src/vue/Frame.java
+++ src/vue/Frame.java
@@ -1,4 +1,5 @@
 package vue;
+
 /******************************************************
 Cours:  LOG121
 Projet: laboratoire #4 
@@ -27,68 +28,50 @@ import backEnd.Controleur;
 import backEnd.ImageModel;
 
 
-public class Frame extends JFrame  {
-	
-	/**
-	 * ?
-	 */
-	private static final long serialVersionUID = 749876921641L;
+public class Frame extends JFrame {
+	private static final long serialVersionUID = 1L;
 	private JPanel panneau = new JPanel();
-	private MenuFenetre menu ;
-	private Controleur ConTemp = null; //ConTemp = controlleur temporaire
-	private ImageModel ImageTampon = null;
-	//private Graphics g;
+	private MenuFenetre menu = new MenuFenetre();
+	private Controleur controleur = Controleur.getControleur();
+	private ImageModel imageModel = new ImageModel();
 
 	public Frame() throws IOException {
-		
+		JPanel labelVignette = new JPanel();
+		JPanel labelMain = new JPanel();
+		JPanel labelPerspective = new JPanel();
+		BoiteVignette boiteVignette = new BoiteVignette(39, 90, 136, 176);
+		Boite boiteMain = new Boite(214, 95, 204, 264);
+		BoitePerspective boitePerspective = new BoitePerspective(457, 95, 204, 264);
+
 		this.setSize(700, 700);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
-		this.setTitle("FenetrePrincipal Lab4");
-		menu = new MenuFenetre();
-		add(menu, BorderLayout.NORTH);
+		this.setTitle("Labo 4");
+		this.add(this.menu, BorderLayout.NORTH);
 
-		panneau = new JPanel();
-		panneau.setLayout(null);
-		JPanel label1 = new JPanel();
-		label1.setBounds(39,70,80,20);
-		label1.add(new JLabel("VIGNETTE"));
-		JPanel label2 = new JPanel();
-		label2.setBounds(214,75,80,20);
-		label2.add(new JLabel("MAIN"));
-		JPanel label3 = new JPanel();
-		label3.setBounds(457,75,120,20);
-		label3.add(new JLabel("PERSPECTIVE"));
-		
-		BoiteVignette BoiteV = new BoiteVignette(39,90,136,176);
-		Boite BoiteMain = new Boite(214,95,204,264);
-		BoitePerspective BoiteP = new BoitePerspective(457,95,204,264);
-		
-		panneau.add(label1);
-		panneau.add(label2);
-		panneau.add(label3);
-		panneau.add(BoiteV);
-		panneau.add(BoiteMain);
-		panneau.add(BoiteP);
-		
-		ConTemp = Controleur.getControleur();
-		try {
-		ImageTampon = new ImageModel();
-		} catch(IOException e) {
-			System.out.println("Pas supposé fail");
-			System.out.println("Si sa a fail ici ben le programme a juste brise");
-			System.exit(0);
-		}
-		ImageTampon.addObserver(BoiteV);
-		ImageTampon.addObserver(BoiteMain);
-		ImageTampon.addObserver(BoiteP);
-		ConTemp.setImage(ImageTampon);
-		ImageTampon.notifyObservers();
+		this.panneau.setLayout(null);
 
-		this.add(panneau,  BorderLayout.CENTER);
-		setVisible(true);
+		labelVignette.setBounds(39,70,80,20);
+		labelVignette.add(new JLabel("VIGNETTE"));
+		labelMain.setBounds(214,75,80,20);
+		labelMain.add(new JLabel("MAIN"));
+		labelPerspective.setBounds(457,75,120,20);
+		labelPerspective.add(new JLabel("PERSPECTIVE"));
+
+		this.panneau.add(labelVignette);
+		this.panneau.add(labelMain);
+		this.panneau.add(labelPerspective);
+		this.panneau.add(boiteVignette);
+		this.panneau.add(boiteMain);
+		this.panneau.add(boitePerspective);
+
+		this.imageModel.addObserver(boiteVignette);
+		this.imageModel.addObserver(boiteMain);
+		this.imageModel.addObserver(boitePerspective);
+		this.controleur.setImage(this.imageModel);
+		this.imageModel.notifyObservers();
+
+		this.add(this.panneau,  BorderLayout.CENTER);
+		this.setVisible(true);
 	}
-	
-
-
 }
