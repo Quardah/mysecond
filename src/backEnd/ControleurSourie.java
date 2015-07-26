1ab3146f32b51ec8e0d4f3f01321de5663749d74
Nicolas Nadeau <niconadeau0@gmail.com>
2015-07-26 19:48:06 -0400

    Nadeau - MouseWheel Update

--- src/backEnd/ControleurSourie.java
+++ src/backEnd/ControleurSourie.java
@@ -3,6 +3,8 @@ package backEnd;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
 import commandes.Commande;
 
 public class ControleurSourie extends MouseAdapter {
@@ -52,6 +54,14 @@ public class ControleurSourie extends MouseAdapter {
 				}
 			}
 		}
+	}
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		Point pos = new Point(controleur.getPositionx(), controleur.getPositiony());
+		Point dimension = new Point();
+		dimension.x = (int)(e.getX()*(0.9f - 1f) + 0.9f*pos.x);
+		dimension.y = (int)(e.getY()*(0.9f - 1f) + 0.9f*pos.y);
+		controleur.setZoomLevel(e.getWheelRotation());
+		controleur.runCommande(Commande.TypeCommande.ZOOMER);
 	}
 
 }
